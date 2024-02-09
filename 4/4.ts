8da26a95e2d558390bee2a1b77131bdc91bcc221
Michal Hiterski <michal.hiterski@comarch.com>
2024-02-09 12:51:06 +0100

    .

--- 4/4.ts
+++ 4/4.ts
@@ -1,11 +1,11 @@
 
 import { fetchData, match, parseData, split, map, log, find, sum, join, reduce, pipe, pairwiseEach, pairwise, flatMap, at, pipec, compose, intersect, filter, take, trim } from '../utils.js';
 
-const length = <T extends { length: number }>(l: T) => l.length;
+const count = <T extends { length: number }>(l: T) => l.length;
 const is = <T>(s: T) => !!s;
-const isLength = compose(length, is);
+const isLength = compose(count, is);
 
-// * 
+// *
 // 31905 too high
 // 21158 right
 pipe(
@@ -24,18 +24,32 @@ pipe(
 );
 
 
-// **
+// fillListWithCardOccurence
+const wstyd = (acc: number[], cur: [string[], string[]], index: number) => {
+  acc[index] = (acc[index] || 0) + 1;
+  for (let i = index + 1; i <= count(intersect(cur)) + index; i++)
+    for (let j = 0; j < acc[index]; j++)
+      acc[i] = (acc[i] || 0) + 1;
 
+  return acc;
+}
+
+
+// map(compose(split(':'), at(1), split('|'), map(l => l.trim().split(' ').filter(isLength)))),
+
+
+// **
 pipe(
   "./4/4p.txt",
   fetchData,
   parseData,
-  map(compose(split(':'), at(1), split('|'), map(l => l.trim().split(' ').filter(isLength)))),
-  map(intersect),
-  filter(isLength),
-  // log
+  map(
+    compose(split(':'), at(1), split('|'),
+      map(compose(trim, split(' '), filter(isLength)))
+    )),
+  reduce(wstyd, []),
+  sum,
+  log
 );
-
-
 
 
